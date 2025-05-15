aa3f7ba2835391a539829cfcd138beb0889d24bf
virajsingh5897 <virajsingh5897@gmail.com>
2025-05-15 23:33:16 +0530

    no shorts

--- mWeb.js
+++ mWeb.js
@@ -18,14 +18,19 @@
 
             if (document.querySelector('[section-identifier="related-items"]') && window.location.href.includes("/watch")) {
                 document.querySelector('[section-identifier="related-items"]').style.display = 'none';
-                console.log('blocked lazy list1')
+                // console.log('blocked lazy list1')
                 clearInterval(interval);
             }
 
             if (document.querySelectorAll('lazy-list').length && window.location.href.includes("/watch")) {
                 document.querySelectorAll('lazy-list').forEach(n => {n.style.display = 'none'});
-                console.log('blocked lazy list2')
+                // console.log('blocked lazy list2')
                 clearInterval(interval);
+            }
+
+            if (window.location.href.includes('/shorts')) {
+                // no shorts
+                history.back();
             }
             
             else {
@@ -42,7 +47,7 @@
     blockHomeRecommendations();
 
     new MutationObserver(function (mutations) {
-        console.log('url changed');
+        // console.log('url changed');
         blockHomeRecommendations();
     }).observe(
         document.querySelector('title'),
